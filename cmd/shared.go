4f752d53040e59de10e339a450a2d8d564a1da29
jkeifer <jkeifer@element84.com>
2023-08-21 09:03:59 -0700

    todo comment about env vars and arg parsing

--- cmd/shared.go
+++ cmd/shared.go
@@ -7,6 +7,11 @@ import (
 	"github.com/spf13/viper"
 )
 
+// TODO: this binds _all_ flags, even those from other sources, like k8s
+// note that we then have 1) undocumented env vars and 2) possible name collisions
+//
+// how do we better document supported env vars (auto-doc env var names in help?)?
+// how do we namespace flags (can flagsets be nested?)?
 func bindFlags(fs *pflag.FlagSet, v *viper.Viper) {
 	fs.VisitAll(func(f *pflag.Flag) {
 		bindFlag(fs, v, f)
